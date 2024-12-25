209e0914d75be79136fc7e2204f53b542cbf1eb1
Fernando Futila <fernando.futila@gmail.com>
2024-12-25 16:04:44 -0300

    feat: add navigation between routes

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -4,13 +4,15 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 
 import { styles } from "./styles";
+import { router } from "expo-router";
+import { Categories } from "@/components/categories";
 
 
 export default function Add() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.back()}>
           <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
         </TouchableOpacity>
 
@@ -19,6 +21,8 @@ export default function Add() {
 
 
       <Text style={styles.label}>Selecione uma categoria</Text>
+
+      <Categories/>
     </View>
   )
 }
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -6,6 +6,7 @@ import { colors } from "@/styles/colors"
 import { Categories } from "@/components/categories"
 import { Link } from "@/components/link"
 import { FlatList } from "react-native"
+import { router } from "expo-router"
 
 export default function Index() {
   return (
@@ -16,7 +17,7 @@ export default function Index() {
           style={styles.logo}
         />
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.navigate("/add")}>
           <MaterialIcons name="add" size={32} color={colors.green[300]}/>
         </TouchableOpacity>
       </View>
